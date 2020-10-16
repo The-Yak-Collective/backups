e875121f08dd623314fc493d8ec2b48f40c253fc
Nathan Acks <github@ecopunk.info>
2020-10-15 19:07:32 -0600

    Quick-and-dirty asset backup

---
+++ .bin/roam_asset_downloader.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# NOTE: Need to install `jq` for this to work.
+
+# Pass in Roam export directory used by roam-to-git
+#
+ROAM_DIR="$1"
+
+# How do we identify the Firebase URLs we want to download?
+#
+FIREBASE_PREFIX="https://firebasestorage.googleapis.com/v0/b/firescript-577a2.appspot.com/o"
+
+# Extract and download Firebase URLs. Note that we only grab one URL per
+# line.
+#
+for FILE_WITH_TOKEN in $(cat "$ROAM_DIR/json"/*.json | jq "." | grep "$FIREBASE_PREFIX/" | sed -e "s#.*$FIREBASE_PREFIX/\([^)}\"]\+\).*#\1#"); do
+	FILE="$(echo "$FILE_WITH_TOKEN" | sed -e 's#\?.*##')"
+	if [[ ! -e "$ROAM_DIR/assets/$FILE" ]]; then
+		curl --location --create-dirs --output "$ROAM_DIR/assets/$FILE" "$FIREBASE_PREFIX/$FILE_WITH_TOKEN"
+	fi
+done
